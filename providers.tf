8b93cc715be042cb6bbda13be0dbc6b18246ea2b
Ahmed Nasr <ahmednasrhassan123@gmail.com>
2023-01-16 16:48:17 +0200

    lab1 terraform

---
+++ providers.tf
@@ -0,0 +1,4 @@
+provider "aws" {
+  shared_credentials_files = ["~/.aws/credentials"]
+  shared_config_files = ["~/.aws/config"]
+}
---
+++ vpc.tf
@@ -0,0 +1,36 @@
+resource "aws_vpc" "nasr-vpc" {
+    cidr_block = "10.0.0.0/16"
+    tags = {
+        "name" : "terraform-vpc"
+    }
+    
+}
+
+resource "aws_subnet" "nasr-subnet" {
+    vpc_id = aws_vpc.nasr-vpc.id
+    cidr_block = "10.0.0.0/24"
+}
+
+resource "aws_internet_gateway" "nasr-gateway" {
+    vpc_id = aws_vpc.nasr-vpc.id
+}
+
+resource "aws_route_table" "nasr-route-table" {
+    vpc_id = aws_vpc.nasr-vpc.id
+    
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.nasr-gateway.id
+  }
+
+  route {
+    ipv6_cidr_block        = "::/0"
+    gateway_id = aws_internet_gateway.nasr-gateway.id
+  }
+
+}
+
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.nasr-subnet.id
+  route_table_id = aws_route_table.nasr-route-table.id
+}
